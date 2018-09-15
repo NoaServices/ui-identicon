6efdcdee3b1a2e5a7afe631911c2c18b45ca8b6e
Tsiry Sandratraina <tsiryid@gmail.com>
2018-09-15 10:09:33 +0300

    Initial Commit

---
+++ src/defaults.ts
@@ -0,0 +1,25 @@
+// Copyright 2016 Dan Finlay
+// Copyright 2017-2018 @polkadot/ui-identicon authors & contributors
+// Copyright 2018 Tsiry Sandratraina
+// This software may be modified and distributed under the terms
+// of the ISC license. See the LICENSE file for details.
+
+const COLORS: Array<string> = [
+  '#AEEA00', // lime
+  '#FC7500', // bright orange
+  '#F50057', // pink
+  '#F73F01', // orangered
+  '#FC1960', // magenta
+  '#C7144C', // raspberry
+  '#18FFFF', // cyan
+  '#1598F2', // lightning blue
+  '#2465E1', // sail blue
+  '#D500F9', // purple
+];
+
+const SHAPE_COUNT = 5;
+
+export {
+  COLORS,
+  SHAPE_COUNT
+};
